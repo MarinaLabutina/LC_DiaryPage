69d25d1b027cfa4dc1152b4436bbe4d5a61d5c50
Marina Labutina <63440386+MarinaLabutina@users.noreply.github.com>
2020-12-01 02:41:08 -0500

    new simple function Create Day Report

--- test/pageobjects/createDayReport.page.js
+++ test/pageobjects/createDayReport.page.js
@@ -39,33 +39,31 @@ class CreateDayReportPage extends BasePage {
   get submitReportBtn() {
     return $$('.ant-btn-primary')[1];
   }
-
   createDailyReport() {
     DiaryPage.createReportBtn.click();
 
     // Marks to your daily report
-    let numberMarks = randomDataCreateDailyReport.checkbox;
-    for (let i = 0; i <= numberMarks; i++) {
-      this.marksCheckbox[i].click();
+    let r;
+    for (let i = 0; i < 12; i++){
+       r = Math.round(Math.random());
+       if (r) { this.marksCheckbox[i].click();}
     }
+
     // What is your moral? level is from 10 to 3
     this.moralField.click();
-    let index = randomDataCreateDailyReport.moralSelectorIndexTop;
-
-    //if (index <= 7) {
-    this.indexLevelMoral[index].click(); //}
-    // else {
-    //  CreateDayReport.indexLevelMoral[7].click();
-    //  CreateDayReport.indexLevelMoral[7].click();
-    //  browser.keys('\uE013');
-    // CreateDayReport.indexLevelMoral[index].waitForDisplayed();
-    // $("#morale_list_1").waitForDisplayed();
-    // CreateDayReport.indexLevelMoral[9].click();
+    r =  Math.ceil(Math.random() * 10);
+    for (let i = 0; i <= r; i++) {
+      browser.keys('\uE013');
+    }
+    browser.keys("Enter");
 
     // How many hours did you study?
     this.hoursField.click();
-    let numberHours = randomDataCreateDailyReport.hoursSelectorIndex;
-    this.hoursList[numberHours].click();
+    r =  Math.ceil(Math.random() * 10);
+    for (let i = 0; i <= r; i++) {
+      browser.keys('\uE013');
+    }
+    browser.keys("Enter");
 
     // How was your day?
     let descriptionDay = randomDataCreateDailyReport.text;
@@ -77,5 +75,43 @@ class CreateDayReportPage extends BasePage {
     return 'Fine'; // вывожу для проверки работы функции,
                   //  НАДО БЫ вернуть все зачения вводимых и выбранных данных для дальнейшей их обработки
   }
+
+  // createDailyReport() {
+  //   DiaryPage.createReportBtn.click();
+  //
+  //   // Marks to your daily report
+  //   let numberMarks = randomDataCreateDailyReport.checkbox;
+  //   for (let i = 0; i <= numberMarks; i++) {
+  //     this.marksCheckbox[i].click();
+  //   }
+  //   // What is your moral? level is from 10 to 3
+  //   this.moralField.click();
+  //   let index = randomDataCreateDailyReport.moralSelectorIndexTop;
+  //
+  //   //if (index <= 7) {
+  //   this.indexLevelMoral[index].click(); //}
+  //   // else {
+  //   //  CreateDayReport.indexLevelMoral[7].click();
+  //   //  CreateDayReport.indexLevelMoral[7].click();
+  //   //  browser.keys('\uE013');
+  //   // CreateDayReport.indexLevelMoral[index].waitForDisplayed();
+  //   // $("#morale_list_1").waitForDisplayed();
+  //   // CreateDayReport.indexLevelMoral[9].click();
+  //
+  //   // How many hours did you study?
+  //   this.hoursField.click();
+  //   let numberHours = randomDataCreateDailyReport.hoursSelectorIndex;
+  //   this.hoursList[numberHours].click();
+  //
+  //   // How was your day?
+  //   let descriptionDay = randomDataCreateDailyReport.text;
+  //   this.descriptionDayField.click();
+  //   this.descriptionDayField.setValue(descriptionDay);
+  //
+  //   this.submitReportBtn.click();
+  //
+  //   return 'Fine'; // вывожу для проверки работы функции,
+  //                 //  НАДО БЫ вернуть все зачения вводимых и выбранных данных для дальнейшей их обработки
+  // }
 }
 export default new CreateDayReportPage();
